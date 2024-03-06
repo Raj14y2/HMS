219397e4e150ddaceb9f6273d9010c910b08cbe7
RAJU <RAJU@LAPTOP-2DHTLVVT>
2024-03-06 13:22:32 +0530

    New classes added

---
+++ src/test/java/sampleTest/Demo5.java
@@ -0,0 +1,19 @@
+package sampleTest;
+
+import org.testng.annotations.Test;
+
+public class Demo5 
+{
+	@Test
+	public void test6()
+	{
+		System.out.println("--testscript3");
+	}
+
+	@Test
+	public void test7()
+	{
+		System.out.println("--testscript4");
+	}
+
+}
